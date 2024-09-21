8122f651b42c32e80a7376d5f16237901d164bbe
KuyaKen <ricewitduhspringroll@gmail.com>
2024-09-20 21:17:56 -0700

    build 1.0.5

--- test.js
+++ test.js
@@ -16,6 +16,8 @@ const oauthNonce = crypto.randomBytes(8).toString('hex');
 
 const URL = `https://api.schoology.com/v1/oauth/request_token?oauth_consumer_key=${Key}&oauth_timestamp=${oauthTimestamp}&oauth_signature_method=PLAINTEXT&oauth_version=1.0&oauth_nonce=${oauthNonce}&oauth_signature=${Secret}%26`;
 
+console.log(URL);
+/*
 axios.get(URL)
   .then(response => {
     console.log('Response:', response.data);
@@ -23,5 +25,6 @@ axios.get(URL)
   .catch(error => {
     console.error('Error making request:', error.response ? error.response.data : error.message);
   });
+*/
 
   
